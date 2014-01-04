de5a4691a0071f3781203ac566bbf2cde6dac4d4
Bytespeicher <kontakt@technikkultur-erfurt.de>
2014-01-04 16:56:59 +0000

    Rewrite to bot class

--- bytebot.py
+++ bytebot.py
@@ -4,39 +4,10 @@ import socket
 import sys
 import re
 import inspect
+import json
 
+from urllib         import urlopen
 from bytebot_config import *
-
-server   = BYTEBOT_SERVER
-channel  = BYTEBOT_CHANNEL
-botnick  = BYTEBOT_NICK
-password = 
-
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print "connecting to:"
-irc.connect((BYTEBOT_SERVER, 6667))
-irc.send("USER "+ BYTEBOT_NICK +" "+ BYTEBOT_NICK +" "+ BYTEBOT_NICK +" :bytespeicher bot\n")
-irc.send("NICK "+ BYTEBOT_NICK +"\n")
-irc.send("PRIVMSG NICKSERV :IDENTIFY " + BYTEBOT_PASSWORD+ "\r\n")
-irc.send("JOIN "+ BYTEBOT_CHANNEL +"\n")
-
-bot = new Bytebot()
-
-while 1:
-    text=irc.recv(2040)
-    print text
-
-    if text.find('PING') != -1:
-        irc.send('PRIVMSG ' + BYTEBOT_CHANNEL + ' :PONG ' + text.split() [1] + '\r\n')
-
-    parse_msg(bot, message)
-
-def parse_msg(bot, message):
-    for method in dir(bot):
-        attr = getattr(bot, method):
-        if inspect.ismethod(attr) and not attr.startswith("_"):
-            print("Executing " + attr + " for " + message)
-            attr(message)
 
 class Bytebot:
     def __init__(self):
@@ -46,29 +17,91 @@ class Bytebot:
         irc.send('PRIVMSG ' + BYTEBOT_CHANNEL + ' :' + message + '\r\n')
 
     def _lookup_dict_command(self, dict):
-	print("dict " + dict)
-        if BYTEBOT_DICT_COMMANDS[dict]:
-            send(BYTEBOT_DICT_COMMANDS[dict])
+	print("lookup dict command: " + dict)
+        return BYTEBOT_DICT_COMMANDS[dict]
 
     def _list_dict_commands(self):
-        for name in BYTEBOT_DICT_COMMANDS:
-            commands += BYTEBOT_DICT_COMMANDS[name]
-        self.send("Available dictionary commands: " + commands)
+        commands = ''
+        for name in sorted(BYTEBOT_DICT_COMMANDS.keys()):
+            commands += name + ', '
+
+        self._send("Available dictionary commands: " + commands)
 
     def ping(self, message):
-        if text.find('PING') != -1:
-            self.send('pong ' + text.split() [0])
+        if text.find('ping') != -1:
+            self._send('pong ' + text.split() [0])
 
     def dict_commands(self, message):
-        dict = re.search('(,,\(.*\))', text)
-        if dict.group(0):
-            answer = self._lookup_dict(dict.group(0))
-            if answer:
-                self._send(answer)
-            else:
-                self._send("Unknown command '" + dict.group(0) + "'")
+        dict = re.search('^.*[: ]!([^ ].[^ $]*)', message)
+        if dict and dict.group(1) and dict.group(1) != 'help':
+            try:
+                answer = self._lookup_dict_command(dict.group(1))
+                if answer:
+                    self._send(answer)
+                else:
+                    self._send("Unknown command '" + dict.group(1) + "'")
+            except Exception, e:
+                self._send("Unknown command '" + dict.group(1) + "'")
 
     def dict_commands_list(self, message):
-        if message == ',,(help)'
+        if message.find('!help') != -1:
+            print('Sending help..')
             self._list_dict_commands()
+
+    def status(self,message):
+        if message.find('!status') != -1:
+            try:
+                response = urlopen(BYTEBOT_STATUS_URL)
+                data = json.loads(response.read())
+
+                self._send('Space status:')
+                if data['state']['open']:
+                    self._send('    Space is currently open: ' + data['state']['message'])
+                else:
+                    self._send('    Space is currently closed ' + data['state']['message'])
+
+            except Exception, e:
+                self._send('Error retrieving Space status')
+                print e
+
+
+
+def parse_msg(bot, text):
+    text = text.split()
+    text.pop(0)
+    text.pop(0)
+    message = " ".join(text)
+
+    for method in dir(bot):
+        attr = getattr(bot, method)
+        if inspect.ismethod(attr) and not attr.__name__.startswith("_"):
+#            print("Executing " + attr.__name__ + " for " + message)
+            attr(message)
+
+
+
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "connecting to:" + BYTEBOT_SERVER
+irc.connect((BYTEBOT_SERVER, 6667))
+irc.send("USER "+ BYTEBOT_NICK +" "+ BYTEBOT_NICK +" "+ BYTEBOT_NICK +" :bytespeicher bot\n")
+irc.send("NICK "+ BYTEBOT_NICK +"\n")
+irc.send("PRIVMSG NICKSERV :IDENTIFY " + BYTEBOT_PASSWORD+ "\r\n")
+irc.send("JOIN "+ BYTEBOT_CHANNEL +"\n")
+
+
+bot = Bytebot()
+
+while 1:
+    text=irc.recv(2040)
+    print text
+
+    if text.find('PING') != -1:
+        print("PING REQUEST:\r\n")
+        print(text)
+        
+        print('Reply will be: "PONG ' + text.split() [1] + '"\r\n')
+        irc.send('PONG ' + text.split() [1] + '\r\n')
+
+    parse_msg(bot, text)
 
