3beb955bf5c5c689cb5c36c638bba895c57bd69f
MKzero <info@linux-web-development.de>
2014-02-05 13:50:22 +0100

    Changed variable name (shorter n stuff..)

--- bytebotpluginloader.py
+++ bytebotpluginloader.py
@@ -12,11 +12,11 @@ class ByteBotPluginLoader:
         plugin_path:    path to plugins
                         MUST NOT end with a trailing slash
         """
-        plugin_path = plugin_path.replace('/', '.')
+        path = plugin_path.replace('/', '.')
         for plugin in plugins:
             try:
                 self.PLUGINS[plugin] = getattr(
-                    __import__("%s.%s" % (plugin_path, plugin)).__dict__[plugin],
+                    __import__("%s.%s" % (path, plugin)).__dict__[plugin],
                     plugin.title()
                 )
             except Exception, e:
