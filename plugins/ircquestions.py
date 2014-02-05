442edfef16da750c1a55267ee72b9eaf1b907c20
MKzero <info@linux-web-development.de>
2014-02-05 13:51:45 +0100

    Changed questions plugin to new plugin format

--- plugins/ircquestions.py
+++ plugins/ircquestions.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-class IRCQuestions:
+from bytebot_config import BYTEBOT_DICT_COMMANDS
+
+class Ircquestions:
     def __init__(self):
         pass
 
-    def lookup_dict_command(self, dict):
-        return BYTEBOT_DICT_COMMANDS[dict]
+    def lookup_dict_command(self, dict_name):
+        return BYTEBOT_DICT_COMMANDS[dict_name]
 
     def list_dict_commands(self):
         commands = ''
@@ -15,19 +17,16 @@ class IRCQuestions:
 
         self._send("Available dictionary commands: " + commands)
 
-    def dict_commands(self, message):
+    def onMessage(self, message, channel):
+        if message.find('!help') != -1:
+            self._list_dict_commands()
+
         dict = re.search('^.*[: ]!([^ ].[^ $]*)', message)
         if dict and dict.group(1) and dict.group(1) not in ['help', 'status']:
             try:
                 answer = self.lookup_dict_command(dict.group(1))
                 if answer:
-                    self._send(answer)
-                else:
-                    self._send("Unknown command '" + dict.group(1) + "'")
+                    self.irc.sendMessage(answer)
             except Exception, e:
                 self._send("Unknown command '" + dict.group(1) + "'")
-
-    def dict_commands_list(self, message):
-        if message.find('!help') != -1:
-            self._list_dict_commands()
 
