1d5f49e646ba646d046ea78d9ae58d1e45027142
MKzero <info@linux-web-development.de>
2014-02-07 16:06:43 +0100

    Changes for messagelogger plugin to fit new plugin architecture

--- plugins/messagelogger.py
+++ plugins/messagelogger.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-class MessageLogger:
-    def __init__(self, file):
-        self.file = file
+from plugin import Plugin
+
+class MessageLogger(Plugin):
+    def __init__(self, filename):
+        self.file = open(filename, "a")
+
+    def __del__(self):
+        self.file.close()
 
     def log(self, message):
         timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
         self.file.write('%s %s\n' % (timestamp, message))
         self.file.flush()
 
-    def close(self):
-        self.file.close()
+    def onConnectionMade(self):
+        self.log("[connected at %s]" %
+                 time.asctime(time.localtime(time.time())))
+
+    def onConnectionLost(self, reason):
+        self.log("[disconnected at %s]", %
+                 time.asctime(time.localtime(time.time())))
+
+    def onSignedOn(self):
+        self.log("[signed on at %s]", %
+                 time.asctime(time.localtime(time.time())))
+
+    def onPrivMsg(self, user, channel, msg):
+        self.log("%s: <%s> %s" % (channel, user, msg))
+
+    def onAction(self, user, channel, msg):
+        self.log("%s: * %s %s" % (channel, user, msg))
+
+    def onIrc_Nick():
+        self.log("%s is now know as %s" % (old_nick, new_nick))
+
