0745f642a710c90c7a71106db65674e92ad29691
Maik Kulbe <info@linux-web-development.de>
2014-05-07 01:42:27 +0200

    Added penis plugin

---
+++ plugins/penis.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from plugins.plugin import Plugin
+from time import time
+
+class penis(Plugin):
+    def __init__(self):
+        pass
+
+    def registerCommand(self, irc):
+        irc.registerCommand('!penis', 'Penis')
+
+    def onPrivmsg(self, irc, msg, channel, user):
+        if msg.find('!penis') == -1:
+            return
+
+        self.irc     = irc
+        self.channel = channel
+
+        try:
+            last_penis = irc.last_penis
+        except Exception as e: 
+            last_penis = 0
+
+        if last_penis < (time() - 300):
+            irc.msg(channel, '  /\)')
+            irc.msg(channel, ' / /')
+            irc.msg(channel, '( Y)')
+            irc.last_penis = time()
+        else:
+            irc.msg(channel, "Don't over it ;)")
