cf8b6a70c1643c537c887987980cb2bdebe29589
MKzero <info@linux-web-development.de>
2014-05-07 09:44:54 +0200

    Added documentation for minuteCron function in plugin interface class

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -17,6 +17,12 @@ class Plugin:
         pass
 
     def minuteCron(self, irc):
+        """Registers a function that will be called every minute. This
+        function must not exceed an execution time of more than 30 seconds to
+        ensure that, when sequentually executed, no ping timeouts occur.
+
+        irc: An instance of the bytebot. Will be passed by the plugin loader
+        """
         pass
 
     def hourCron(self, irc):
