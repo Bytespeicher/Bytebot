536eb55c899b3e2e4797beb384a670dd9c4c63b3
MKzero <info@linux-web-development.de>
2014-05-07 08:58:30 +0200

    Added/updated docs for plugin loader class

--- bytebotpluginloader.py
+++ bytebotpluginloader.py
@@ -4,11 +4,18 @@
 from sys                import exit
 
 class ByteBotPluginLoader:
+    """This class enables automatic loading and method calling for plugin 
+    classes. 
+
+    The class also catches faulty plugin code so if an exception is thrown 
+    that is not handled in the plugin code itself it will be handled here 
+    without crashing the whole bot."""
+
     PLUGINS = {} 
 
     def __init__(self, plugins, plugin_path='plugins'):
         """
-        plugins:        tupel with plugins
+        plugins:        tupel with plugin names to register
         plugin_path:    path to plugins
                         MUST NOT end with a trailing slash
         """
@@ -25,6 +32,11 @@ class ByteBotPluginLoader:
                 exit(255)
 
     def run(self, fn, args={}):
+        """Runs a specific function on all registered plugins
+
+        fn              plugin method name to execute
+        args            dictionary with arguments to call the method with
+        """
         for key, plugin in self.PLUGINS.iteritems():
             try:
                 method = getattr(plugin, fn)
