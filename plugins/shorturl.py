e5a864fa8a292d2ef55f18894db83ba3ab50374a
Jurkov <christoph@jurko.de>
2015-10-14 00:26:37 +0200

    format changed to PEP 0008

--- plugins/shorturl.py
+++ plugins/shorturl.py
@@ -6,7 +6,7 @@ import json
 import re
 
 from plugins.plugin import Plugin
-from bs4            import BeautifulSoup
+from bs4 import BeautifulSoup
 from twisted.python import log
 
 from clarifai.client import ClarifaiApi
@@ -14,20 +14,23 @@ from clarifai.client import ClarifaiApi
 from bytebot_config import BYTEBOT_HTTP_TIMEOUT, BYTEBOT_HTTP_MAXSIZE
 from bytebot_config import BYTEBOT_PLUGIN_CONFIG
 
+
 class shorturl(Plugin):
+
     def googl(self, url):
         log.msg("Shortening URL %s" % url)
 
         post_url = 'https://www.googleapis.com/urlshortener/v1/url'
         postdata = {'longUrl': url}
-        headers = {'Content-Type':'application/json'}
+        headers = {'Content-Type': 'application/json'}
 
         req = urllib2.Request(
             post_url,
             json.dumps(postdata),
             headers
         )
-        data = urllib2.urlopen(url=req, timeout=BYTEBOT_HTTP_TIMEOUT).read(BYTEBOT_HTTP_MAXSIZE)
+        data = urllib2.urlopen(url=req, timeout=BYTEBOT_HTTP_TIMEOUT).read(
+            BYTEBOT_HTTP_MAXSIZE)
         ret = json.loads(data)[u'id'].encode('ascii', 'ignore')
 
         return ret
@@ -40,14 +43,15 @@ class shorturl(Plugin):
             'url': url,
             'key': BYTEBOT_PLUGIN_CONFIG['shorturl']['api_key']
         }
-        headers = {'Content-Type':'application/json'}
+        headers = {'Content-Type': 'application/json'}
 
         req = urllib2.Request(
             post_url,
             json.dumps(postdata),
             headers
         )
-        data = urllib2.urlopen(url=req, timeout=BYTEBOT_HTTP_TIMEOUT).read(BYTEBOT_HTTP_MAXSIZE)
+        data = urllib2.urlopen(url=req, timeout=BYTEBOT_HTTP_TIMEOUT).read(
+            BYTEBOT_HTTP_MAXSIZE)
         ret = json.loads(data)[u'url_short'].encode('ascii', 'ignore')
 
         return ret
@@ -56,12 +60,13 @@ class shorturl(Plugin):
         data = urllib2.urlopen(url=url, timeout=BYTEBOT_HTTP_TIMEOUT)
         soup = BeautifulSoup(data)
         return soup.title.getText().encode('utf-8')[:60]
-    
+
     def getTags(self, url):
         ret = ''
-        api = ClarifaiApi(BYTEBOT_PLUGIN_CONFIG['shorturl']['clarifai_app_id'], BYTEBOT_PLUGIN_CONFIG['shorturl']['clarifai_app_secret'])
+        api = ClarifaiApi(BYTEBOT_PLUGIN_CONFIG['shorturl'][
+                          'clarifai_app_id'], BYTEBOT_PLUGIN_CONFIG['shorturl']['clarifai_app_secret'])
         tags = api.tag_image_urls(url)
-        
+
         if(tags[u'status_code'] == "OK"):
             ret = ', '.join(tags[u'results'][0][u'result'][u'tag'][u'classes'])
 
@@ -70,17 +75,20 @@ print ret
     def onPrivmsg(self, irc, msg, channel, user):
 
         try:
-            url = re.findall('http[s]?://(?:[^\s]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)[0]
+            url = re.findall(
+                'http[s]?://(?:[^\s]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)[0]
             try:
-                short_function = getattr(self, BYTEBOT_PLUGIN_CONFIG['shorturl']['shortener'])
+                short_function = getattr(self, BYTEBOT_PLUGIN_CONFIG[
+                                         'shorturl']['shortener'])
                 shorturl = short_function(url)
             except NameError:
-                log.msg('Could not load shortener function ' + BYTEBOT_PLUGIN_CONFIG)
+                log.msg('Could not load shortener function ' +
+                        BYTEBOT_PLUGIN_CONFIG)
         except Exception as e:
             log.msg(e)
             return
 
-        if(url[-4:]=='.jpg'):
+        if(url[-4:] == '.jpg'):
             try:
                 desc = 'Tags : ' + self.getTags(url)
             except Exception as e:
