74257a783bb0c902a87f7ffe914afc3b259871c5
Maik Kulbe <info@linux-web-development.de>
2015-10-31 01:42:17 +0100

    Added userlist. Fixes #11

--- plugins/spacestatus.py
+++ plugins/spacestatus.py
@@ -14,6 +14,10 @@ class spacestatus(Plugin):
             '!status',
             'Returns the door status of the hackerspace rooms'
         )
+        irc.registerCommand(
+            '!users',
+            'Returns the current users inside the hackerspace rooms'
+        )
 
     def onPrivmsg(self, irc, msg, channel, user):
         if msg.startswith('!status'):
@@ -28,3 +32,16 @@ class spacestatus(Plugin):
                     irc.msg(channel, '\tDer Space ist geschlossen!')
             except Exception:
                 irc.msg(channel, '\tFehler beim Abrufen des Status')
+
+        if msg.startswith('!users'):
+            try:
+                response = urlopen(BYTEBOT_PLUGIN_CONFIG['spacestatus']['url'])
+                data = json.loads(response.read())['sensors'][
+                    'people_now_present']
+
+                if data['value'] > 0:
+                    irc.msg(channel, 'Space users: ' +
+                            str(', '.join(data['names'])))
+            except Exception as e:
+                print(e)
+                irc.msg(channel, '\tFehler beim Abrufen der User')
