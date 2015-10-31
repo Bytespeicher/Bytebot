5d030af641b755a19e920f7c3c2e7152531f72b7
Maik Kulbe <info@linux-web-development.de>
2015-10-31 01:41:42 +0100

    PEP8ify

--- plugins/spacestatus.py
+++ plugins/spacestatus.py
@@ -3,13 +3,17 @@
 
 from bytebot_config import BYTEBOT_PLUGIN_CONFIG
 from plugins.plugin import Plugin
-from urllib         import urlopen
+from urllib import urlopen
 
 import json
 
+
 class spacestatus(Plugin):
     def registerCommand(self, irc):
-        irc.registerCommand('!status', 'Returns the door status of the hackerspace rooms')
+        irc.registerCommand(
+            '!status',
+            'Returns the door status of the hackerspace rooms'
+        )
 
     def onPrivmsg(self, irc, msg, channel, user):
         if msg.startswith('!status'):
@@ -22,5 +26,5 @@ class spacestatus(Plugin):
                     irc.msg(channel, '\tDer Space ist offen!')
                 else:
                     irc.msg(channel, '\tDer Space ist geschlossen!')
-            except Exception as e:
+            except Exception:
                 irc.msg(channel, '\tFehler beim Abrufen des Status')
