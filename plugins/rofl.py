bef2473db26606db35ece02eff6fb91ab752d7ca
Jurkov <christoph@jurko.de>
2016-11-08 22:52:21 +0100

    redesigned broken roflcopter

--- plugins/rofl.py
+++ plugins/rofl.py
@@ -5,17 +5,17 @@ from irc3 import asyncio
 
 @command(permission="view")
 @asyncio.coroutine
-def parking(bot, mask, target, args):
+def rofl(bot, mask, target, args):
     """Show the current rofl rofl rofl
 
         %%rofl
     """
     bot.privmsg(target, 'Incoming ROFLCOPTER!')
-    bot.privmsg(target, ' ')
+    bot.privmsg(target, '.')
     bot.privmsg(target, '    ROFL:ROFL:ROFL:ROFL')
     bot.privmsg(target, '         ___^___ _')
-    bot.privmsg(target, ' L    __ / [] \')
-    bot.privmsg(target, 'LOL == =__           \')
+    bot.privmsg(target, ' L    __/      [] \\')
+    bot.privmsg(target, 'LOL == =__         \\')
     bot.privmsg(target, ' L      \___ ___ ___]')
     bot.privmsg(target, '              I   I')
     bot.privmsg(target, '          ----------/')
