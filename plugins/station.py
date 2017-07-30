13ae514ece324143363f037a63b62ae7c3fb6098
Martin Zimmermann <zimmermann@weloveapps.de>
2017-07-30 21:28:26 +0200

    implement new API to retrieve departures for a given station
    * support for SOLL + IST (aka »real-time«) on a minute basis
    * improve error handling
    * include more Python 3 idioms
    
    Output for Erfurt, Leipziger Platz:
    
        Erfurt, Leipziger Platz
        19:26 | Tram 2 -> P+R-Platz Messe
        19:33 | Tram 2 -> Ringelberg
        19:40 | Bus  9 -> Daberstedt
        19:48 | Bus  9 -> Nordbahnhof
        19:56 | Tram 2 -> P+R-Platz Messe
        20:03 | Tram 2 -> Ringelberg
        20:10 | Bus  9 -> Daberstedt
        20:18 | Bus  9 -> Nordbahnhof
        20:26 | Tram 2 -> P+R-Platz Messe
        20:33 | Tram 2 -> Ringelberg
    
    Output for Erfurt, Hauptbahnhof (IST != SOLL):
    
      Erfurt, Leipziger Platz
      19:25 +3 | Tram 3 -> Europaplatz
      19:26 | Tram 1 -> Rieth, Gleis 1
      ...

--- plugins/station.py
+++ plugins/station.py
@@ -1,64 +1,72 @@
-﻿from irc3.plugins.command import command
-
 from irc3 import asyncio
+from irc3.plugins.command import command
+
+import re
 import json
 import aiohttp
-import datetime
-import time
+
+from datetime import datetime
+
+
+def remove_component(targetlocation, component="Erfurt"):
+    """
+    Fix API locations like "Erfurt, Daberstedt" to just "Daberstedt"
+    """
+    return re.sub("(,\\s*|\\s*-\\s*)" + component + "|" + component + "((,\\s*)|\\s*-\\s*)", "", targetlocation)
 
 
 @command(permission="view")
 @asyncio.coroutine
 def station(bot, mask, target, args):
-    """Show current tram and bus station information
-
-        %%station
     """
-    url = "https://wla.31384.de:4044/evag/functions/departures"
-    payload = {"extId": "151213", "useTimestamp": True}
+    Show departures for station: 151213 (Leipziger Platz, Erfurt). The service
+    URL is consumed by Erfurt Mobil (Android/iOS).
+
+    A list of all known stations in the VMT area can be viewed here:
+    https://evag-live.wla-backend.de/stations/latest.json
+
+    %%station
+    """
+    url = "https://evag-live.wla-backend.de/node/v1/departures/151213"
     headers = {"Content-Type": "application/json"}
     with aiohttp.Timeout(10):
         with aiohttp.ClientSession(loop=bot.loop) as session:
-            resp = yield from session.post(url, data=json.dumps(payload),
-                                           headers=headers)
+            resp = yield from session.get(url, headers=headers)
             if resp.status != 200:
                 bot.privmsg(target, "Error while retrieving traffic data.")
                 raise Exception()
-            r = yield from resp.read()
+            body = yield from resp.read()
 
     try:
-        j = json.loads(r.decode('utf-8'))
-        departures = j["result"]["departures"]
+        body = json.loads(body.decode('utf-8'))
+    except json.JSONDecodeError:
+        bot.privmsg(target, "Error while retrieving traffic data.")
 
+    try:
         data = []
-        count = 0
+        for departure in body.get("departures", []):
+            delay = 0  # in seconds
+            try:
+                delay = max(0, departure.get("timestamp_rt", 0) - departure["timestamp"]) // 60
+            except ValueError:
+                pass
 
-        for i in range(len(departures)):
-            if(j["result"]["departures"][i]["timestamp"] >= time.time()):
-                data.append(
-                    {'departure': j["result"]["departures"][i]["timestamp"],
-                     'type': j["result"]["departures"][i]["type"],
-                     'targetLocation':
-                         j["result"]["departures"][i]["targetLocation"],
-                     'line': j["result"]["departures"][i]["line"]})
-            if(count > 10):
-                break
-            else:
-                count = count + 1
+            data.append({
+                "type": "Tram" if departure["type"] == "Strab" else "Bus",
+                "line": departure["line"],
+                "target": remove_component(departure["targetLocation"]),
+                "time": datetime.utcfromtimestamp(departure["timestamp"]), "delay": delay
+            })
 
-        bot.privmsg(target, "Erfurt, Leipziger Pl.")
-        for i in range(len(data)):
+        bot.privmsg(target, "Erfurt, Leipziger Platz")
+        for departure in data[:10]:
             bot.privmsg(
                 target,
-                "%s: %4.4s %s -> %s" % (
-                    datetime.datetime.fromtimestamp(
-                        data[i]['departure']
-                    ).strftime('%H:%M'),
-                    data[i]['type'].upper(),
-                    data[i]['line'],
-                    data[i]['targetLocation'],
-                )
-            )
+                "{:4}{} | {:4} {} -> {}".format(
+                    departure["time"].strftime("%H:%M"),
+                    " +{:d}".format(departure["delay"]) if departure["delay"] else "",
+                    departure["type"], departure["line"],
+                    departure["target"]))
 
     except KeyError:
         bot.privmsg(target, "Error while retrieving traffic data.")
