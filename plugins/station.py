dcfd8fcc35c3c13d985d0facba2b5ef4ec2b97fd
Marcel Pennewiss <github@pennewiss.de>
2017-12-27 21:40:50 +0100

    Auto-caching station list and time based filter
    Add auto-caching station list (fixes #186) and add
    time based filter (fixes #87).

--- plugins/station.py
+++ plugins/station.py
@@ -1,38 +1,57 @@
 from irc3 import asyncio
 from irc3.plugins.command import command
+from irc3.plugins.cron import cron
 
 import io
 import re
 import json
 import aiohttp
 import os
+import pytz
+import datetime
 
-from datetime import datetime
 from dateutil import tz
 
 from difflib import get_close_matches
 
-URL = "https://evag-live.wla-backend.de/node/v1/departures/{extId}"
 RE_FIX = "(,\\s*|\\s*-\\s*){component}|{component}((,\\s*)|\\s*-\\s*)"
-STATION_FILE = os.path.dirname(os.path.realpath(__file__)) + \
-                   '/../data/stations.latest.json'
 
 
-def lookup_by_id(extId):
+def station_configuration(bot):
+    """Load configuration"""
+    config = {
+        'cache': '/tmp/station.cache',
+        'city': 'Erfurt',
+        'id': '151213',
+        'announce_minutes': 30
+    }
+    config.update(bot.config.get(__name__, {}))
+    return config
+
+
+def lookup_by_id(bot, extId):
     """
     Lookup station by extId and return the pretty concatenation of name, parent
     """
-    with io.open(STATION_FILE) as fp:
+
+    # load configuration
+    config = station_configuration(bot)
+
+    with io.open(config['cache']) as fp:
         for obj in json.load(fp):
             if obj["id"] == extId:
                 return "{parent}, {name}".format(**obj)
 
 
-def lookup_by_name(name, parent):
+def lookup_by_name(bot, name, parent):
     """
     Lookup station by (parent, name) and return its extId.
     """
-    with io.open(STATION_FILE) as fp:
+
+    # load configuration
+    config = station_configuration(bot)
+
+    with io.open(config['cache']) as fp:
         data = json.load(fp)
         candidates = [obj["name"] for obj in data if obj["parent"] == parent]
 
@@ -67,34 +86,47 @@ def station(bot, mask, target, args):
     """
 
     # load configuration
-    config = bot.config.get(__name__, {})
+    config = station_configuration(bot)
+    if " ".join(args['<name>']) == "help":
+        return \
+            "Use !station [minutes] NAME/EXTID to get recent departures " + \
+            "for the next minutes (default: %s)" % config['announce_minutes']
 
-    city = config.get("city")
-    if not city:
-        city = "Erfurt"
+    if not os.path.isfile(config['cache']):
+        yield from _update_cache(bot)
+        return "Station cache is unavailable. " + \
+               "Please try again in a few minutes!"
+
+    if 'city' not in config:
+        bot.privmsg(target, "No city configured.")
+        return
 
     # determine extId (VMT internal id) and friendly name
+    announce_limit_arg = None
+    if len(args['<name>']) >= 2 and args['<name>'][0].isdigit():
+        announce_limit_arg = int(args['<name>'][0])
+        del args['<name>'][0]
+
     extid = " ".join(args["<name>"])
     if not extid:
-        extid = str(config.get("id", 151213))
-
-    if extid == "help":
-        return "Use !station NAME/EXTID to get recent departures"
+        extid = str(config['id'])
 
     name = None
     if extid.isdigit():
-        name = lookup_by_id(extid)
+        name = lookup_by_id(bot, extid)
     else:
-        extid = lookup_by_name(extid, city)
+        extid = lookup_by_name(bot, extid, config['city'])
         if extid is not None:
-            name = lookup_by_id(extid)
+            name = lookup_by_id(bot, extid)
 
     if extid is None:
         return "Unknown station: " + " ".join(args["<name>"])
 
     with aiohttp.Timeout(10):
         with aiohttp.ClientSession(loop=bot.loop) as session:
-            resp = yield from session.get(URL.format(extId=extid))
+            resp = yield from session.get(
+                       config['url_departures'].format(extId=extid)
+                   )
             if resp.status != 200:
                 bot.privmsg(target, "Error while retrieving traffic data.")
                 raise Exception()
@@ -109,26 +141,39 @@ def station(bot, mask, target, args):
         # use local timezone
         tzinfo = tz.tzlocal()
 
+        announce_limit = datetime.datetime.now(tzinfo)
+        if announce_limit_arg is not None:
+            announce_limit += datetime.timedelta(minutes=announce_limit_arg)
+        else:
+            announce_limit += datetime.timedelta(
+                                  minutes=config['announce_minutes'])
+
         data = []
         for departure in body.get("departures", []):
             delay = 0  # in seconds
             try:
-                timestamp = departure.get("timestamp")
-                timestamp_rt = departure.get("timestamp_rt", 0)
-                delay = max(0, timestamp_rt - timestamp) // 60
+                timestamp = datetime.datetime.fromtimestamp(
+                                departure.get("timestamp"), tzinfo)
+                timestamp_rt = datetime.datetime.fromtimestamp(
+                                   departure.get("timestamp_rt", 0), tzinfo)
+                delay = max(0, (timestamp_rt - timestamp).seconds) // 6
             except ValueError:
                 pass
+
+            # limit output to announce_minutes
+            if timestamp > announce_limit:
+                break
 
             data.append({
                 "type": "Tram" if departure["type"] == "Strab" else "Bus",
                 "line": departure["line"],
-                "target": remove_component(departure["targetLocation"], city),
-                "time": datetime.fromtimestamp(departure["timestamp"], tzinfo),
+                "target": remove_component(
+                    departure["targetLocation"],
+                    config['city']
+                ),
+                "time": timestamp,
                 "delay": delay
             })
-
-        # limit output to 10 max
-        data = data[:10]
 
         # add padding to all departures if any is delayed
         delayed = any(map(lambda d: d["delay"] > 0, data))
@@ -156,3 +201,69 @@ def station(bot, mask, target, args):
     except KeyError:
         bot.privmsg(target, "Error while retrieving traffic data.")
         raise Exception()
+
+
+@cron('23 0 * * *')
+@asyncio.coroutine
+def cccongress_update_cron(bot):
+    """Update station list once a day"""
+
+    yield from _update_cache(bot, 1)
+
+
+@asyncio.coroutine
+def _update_cache(bot, cron=0):
+    """Update cached station list
+
+        cron: defines whether update was triggered by cron or not
+    """
+
+    config = station_configuration(bot)
+    config['cache_etag'] = config['cache'] + '.etag'
+
+    if os.path.isfile(config['cache']):
+        """Try to read read cache information about last run"""
+        cache = open(config['cache_etag'], "r")
+        headers = {'If-None-Match': cache.readline().strip().rstrip('\n')}
+    else:
+        headers = {}
+
+    try:
+        """Request the station list"""
+        with aiohttp.Timeout(30):
+            with aiohttp.ClientSession(loop=bot.loop) as session:
+                resp = yield from session.get(
+                           config['url_stationlist'],
+                           headers=headers
+                       )
+                if resp.status == 200:
+                    """Get text content and etag from http request."""
+                    r = yield from resp.text()
+                    r_etag = resp.headers.get('etag')
+                elif resp.status == 304:
+                    """Etag was used in request and noting changed."""
+                    return
+                else:
+                    raise Exception()
+
+    except Exception as e:
+        bot.log.error(e)
+        if cron == 0:
+            bot.privmsg(bot.config.autojoins[0],
+                        "Error while retrieving station list")
+
+    try:
+        """ Save schedule cache to disk """
+        cache = open(config['cache'], "w")
+        cache.truncate(0)
+        cache.write('%s' % r)
+        cache.close()
+
+        """ Save etag cache to disk """
+        cache = open(config['cache_etag'], "w")
+        cache.truncate(0)
+        cache.write('%s' % r_etag)
+        cache.close()
+
+    except OSError as e:
+        bot.log.error(e)
